a9fc5133c375cf68f2c329dac7e8b4ac02c8402b
Bibhuti <biswalbibhuti8@gmail.com>
2023-01-11 14:33:16 +0530

    first submission

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    B obj=new B();
+    obj.meth();
+    // System.out.println(obj.meth());
+  }
+}
+class A{
+    public String meth(){
+        String str="Invoking method from class A";
+        return str;
+    }
+}
+class B extends A{
+    public String meth(){
+        String str="Method is overridden in Extendend class B";
+        return str;
+    }
+
 }
