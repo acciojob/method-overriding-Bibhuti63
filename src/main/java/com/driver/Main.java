47c46777a90f8b3c6c11a0d25dfe03c4aad2a784
Bibhuti <biswalbibhuti8@gmail.com>
2023-01-12 11:38:22 +0530

    changed something

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,10 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-    B obj=new B();
-    obj.meth();
-    // System.out.println(obj.meth());
+   A obj1=new B();
+   obj1.meth();
+   B obj2=new B();
+   obj2.meth();
   }
 }
 class A{
