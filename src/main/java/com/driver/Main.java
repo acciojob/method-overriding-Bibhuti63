25be8ea4dadc6be970c4840cc7e9d2eefb92f6bf
Bibhuti <biswalbibhuti8@gmail.com>
2023-01-12 11:55:14 +0530

    4th commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,10 +2,13 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-   A obj1=new B();
+   A obj1=new A();
    obj1.meth();
+//    System.out.println(obj1.meth());
+
    B obj2=new B();
    obj2.meth();
+//    System.out.println(obj2.meth());
   }
 }
 class A{
