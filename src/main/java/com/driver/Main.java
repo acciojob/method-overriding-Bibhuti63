61c94bba13dab0c3c30f841050465d3203f01119
Bibhuti <biswalbibhuti8@gmail.com>
2023-01-13 11:03:47 +0530

    Changed in main class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,25 +2,25 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-   A obj1=new A();
-   obj1.meth();
+//   A obj1=new A();
+//   obj1.meth();
 //    System.out.println(obj1.meth());
 
-   B obj2=new B();
+   A obj2=new B();
    obj2.meth();
 //    System.out.println(obj2.meth());
   }
-}
-class A{
-    public String meth(){
-        String str="Invoking method from class A";
-        return str;
+    public static class A{
+        public String meth(){
+            String str="Invoking method from class A";
+            return str;
+        }
     }
-}
-class B extends A{
-    public String meth(){
-        String str="Method is overridden in Extendend class B";
-        return str;
-    }
+    public static class B extends A{
+        public String meth(){
+            String str="Method is overridden in Extendend class B";
+            return str;
+        }
 
+    }
 }
